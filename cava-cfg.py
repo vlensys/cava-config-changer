85338dab02054fe4a6123ac278d68cb2df20f975
vlensys <monkeeeloll@gmail.com>
2025-08-02 21:35:34 +0300

    added asking for ur home directory

--- cava-cfg.py
+++ cava-cfg.py
@@ -1,11 +1,26 @@
 import subprocess
 import shutil
 import os
+import json
 
-source_eva = "/home/valen/cava-configs/eva/config"
-source_yk = "/home/valen/cava-configs/yk/config"
-source_uhh = "/home/valen/cava-configs/uhh/config"
-dest = "/home/valen/.config/cava/config"
+config_path = "config.json"
+
+if not os.path.exists(config_path):
+    home_dir = input("homedir pls")
+    config = {"home_dir": home_dir}
+    with open(config_path, "w") as f:
+        json.dump(config, f, indent=4)
+
+with open(config_path, "r") as f:
+    config = json.load(f)
+
+home_dir = config["home_dir"]
+print(home_dir)
+
+source_eva = f"{home_dir}/cava-configs/eva/config"
+source_yk = f"{home_dir}/cava-configs/yk/config"
+source_uhh = f"{home_dir}/cava-configs/uhh/config"
+dest = f"{home_dir}/.config/cava/config"
 
 cava_path = "/home/valen/.config/cava/config"
 cmd = "caelestia wallpaper"
